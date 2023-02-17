8ae104c97d8de406062e5fce1e2104f9515ad1f6
Adam <Adam.Mohsin@city.ac.uk>
2023-02-17 15:09:02 +0000

    Initial Commit

---
+++ Study+/Screens/Progress Screen.swift
@@ -0,0 +1,20 @@
+//
+//  Progress Screen.swift
+//  Study+
+//
+//  Created by Adam Mohsin on 17/02/2023.
+//
+
+import SwiftUI
+
+struct Progress_Screen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Progress_Screen_Previews: PreviewProvider {
+    static var previews: some View {
+        Progress_Screen()
+    }
+}
