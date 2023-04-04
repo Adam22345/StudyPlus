38f034daba4d7de0fed4ed648521173da07fecf8
Adam <Adam.Mohsin@city.ac.uk>
2023-04-04 13:53:25 +0100

    code improvments

--- Study+/Study_App.swift
+++ Study+/Study_App.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-//App is executed here
+
 enum Screen{
     case calendar
     case progress
@@ -23,43 +23,32 @@ final class manageTabs: ObservableObject {
     }
 }
 
-
+//App is executed here
 @main
+//This code is the main point of entry of the App
 struct Study: App {
+    // These StateObjects store and retrieve users subjects
     @StateObject private var store = SubjectStorage()
-    @StateObject var theSubjects = SubjectCreate(view: true)
+    @StateObject var theSubjects = SubjectCreate(view: true) // create new subjects
     
-    
+    // the body of the app used to dispaly the views
     var body: some Scene {
         WindowGroup {
-          
-                
-               
-            
-    
-            
-            NavigationView{
-                ScheduleListScreen(subjects: $store.subjects) //Needs to be fixed
+          NavigationView{
+                ScheduleListScreen(subjects: $store.subjects)
                     .environmentObject(theSubjects)
-                
-                
-                
-                
-                
                 
                 ScheduleView(subjects: $store.subjects){
                     SubjectStorage.store(subjects: store.subjects) { result in
                         if case .failure(let error) = result {
                             fatalError(error.localizedDescription)
-                            
-                            
                         }
                     }
                 }
             }
            
-           
-            .onAppear {
+            //code for error handling
+           .onAppear {
                 SubjectStorage.load { result in
                     switch result {
                     case .failure(let error):
@@ -74,54 +63,3 @@ struct Study: App {
         }
     }
 }
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-  //  @StateObject var router = manageTabs()
-    
-   // var body: some Scene {
-     //   WindowGroup {
-       //     TabView(selection: $router.screen){
-         //       CalendarScreen()
-           //         .badge(10)
-             //       .tag(Screen.calendar)
-               //     .environmentObject(router)
-                 //   .tabItem {
-                   //     Label("Calendar",systemImage: "calendar")
-       //             }
-         //       ProgressScreen()
-           //         .tag(Screen.progress)
-             //       .environmentObject(router)
-               //     .tabItem{
-                 //       Label("Progress",systemImage: "chart.bar.xaxis")
-         //           }
-            
-        //    }
-       // }
- 
-//}
-
